311f40b02cacb211c43a271dcc528b76b085d363
huangchuhua <chuhua_huang@126.com>
2017-09-20 19:38:53 +0800

    完成首页导航区的样式

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -10,6 +10,49 @@
 
         </mt-swipe>
 
+        <!-- 导航区 六空格 基本MUI grid-default.html-->
+        <div class="mui-content">
+            <ul class="mui-table-view mui-grid-view mui-grid-9">
+                <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                    <a href="#">
+                        <img src="../../images/menu1.png" alt="">
+                        <div class="mui-media-body">新闻资讯</div>
+                    </a>
+                </li>
+                <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                    <a href="#">
+                        <img src="../../images/menu2.png" alt="">
+                        <div class="mui-media-body">图片分享</div>
+                    </a>
+                </li>
+                <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                    <a href="#">
+                        <img src="../../images/menu3.png" alt="">
+                        <div class="mui-media-body">商品购买</div>
+                    </a>
+                </li>
+                <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                    <a href="#">
+                        <img src="../../images/menu4.png" alt="">
+                        <div class="mui-media-body">留言反馈</div>
+                    </a>
+                </li>
+                <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                    <a href="#">
+                        <img src="../../images/menu5.png" alt="">
+                        <div class="mui-media-body">视频专区</div>
+                    </a>
+                </li>
+                <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                    <a href="#">
+                        <img src="../../images/menu6.png" alt="">
+                        <div class="mui-media-body">联系我们</div>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+
     </div>
 </template>
 
@@ -48,6 +91,26 @@ export default {
             height: 100%;
         }
     }
+}
+
+// 导航区
+.mui-table-view-cell {
+    img {
+        width: 60px;
+    }
+}
+
+.mui-table-view.mui-grid-view .mui-table-view-cell .mui-media-body {
+    font-size: 14px;
+}
+
+.mui-grid-view.mui-grid-9 {
+    background-color: #fff;
+    border: 0;
+}
+
+.mui-grid-view.mui-grid-9 .mui-table-view-cell {
+    border: 0;
 }
 </style>
 
